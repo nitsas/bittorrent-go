d711906606ef2fde6b2b0a6a7510e44bea805feb
Chris Nitsas <nitsas@skroutz.gr>
2024-01-27 20:28:33 +0200

    Refactor string and integer parsing, return ending index

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -6,66 +6,82 @@ import (
 	"os"
 	"strconv"
 	"unicode"
-	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
 // Examples:
+// - "0:" -> ""
 // - "5:hello" -> "hello"
 // - "i52e" -> 52
-func decodeBencode(bencodedString string) (interface{}, error) {
-	if unicode.IsDigit(rune(bencodedString[0])) {
-		return decodeBencString(bencodedString)
-	} else if rune(bencodedString[0]) == 'i' {
-		return decodeBencInt(bencodedString)
+func decodeBencode(bencData string) (interface{}, error) {
+	token, _, error := decodeNextBencToken(bencData[0:])
+	return token, error
+}
+
+func decodeNextBencToken(bencData string) (interface{}, uint, error) {
+	if unicode.IsDigit(rune(bencData[0])) {
+		return decodeBencString(bencData)
+	} else if rune(bencData[0]) == 'i' {
+		return decodeBencInt(bencData)
 	} else {
-		return "", fmt.Errorf("Only strings and integers are supported at the moment")
+		return "", 0, fmt.Errorf("Unrecognized type")
 	}
 }
 
 // Example:
+// - "0:" -> ""
 // - "5:hello" -> "hello"
 // - "10:hello12345" -> "hello12345"
-func decodeBencString(bencodedString string) (string, error) {
-	var firstColonIndex int
+func decodeBencString(bencData string) (string, uint, error) {
+	var colonIndex int
 
-	for i := 0; i < len(bencodedString); i++ {
-		if bencodedString[i] == ':' {
-			firstColonIndex = i
+	for i, c := range bencData {
+		if c == ':' {
+			colonIndex = i
 			break
 		}
 	}
 
-	lengthStr := bencodedString[:firstColonIndex]
-
+	lengthStr := bencData[:colonIndex]
 	length, err := strconv.Atoi(lengthStr)
 	if err != nil {
-		return "", err
+		return "", 0, err
 	}
+	nextIndex := uint(colonIndex + 1 + length)
 
-	return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
+	return bencData[colonIndex+1 : nextIndex], nextIndex, nil
 }
 
 // Examples:
 // - "i52e" -> 52
 // - "i-42e" -> -42
-func decodeBencInt(bencodedString string) (int64, error) {
-	lastRune := rune(bencodedString[len(bencodedString)-1])
-	if lastRune != 'e' {
-		return 0, fmt.Errorf("Bencoded integer not terminated properly: %q instead of 'e'", lastRune)
+func decodeBencInt(bencData string) (int, uint, error) {
+	nextIndex := len(bencData) + 1
+	for i, c := range bencData {
+		if c == 'e' {
+			nextIndex = i + 1
+			break
+		}
 	}
-	// Here are some extra invalid cases that Atoi can handle sanely:
-	// "i-0e": -0 is invalid
-	// "i03e": leading 0s are invalid, except for "i0e"
+	if nextIndex > len(bencData) {
+		err := fmt.Errorf("Bencoded integer '%s' missing trailing 'e'", bencData)
+		return 0, 0, err
+	}
 
+	// Here are some extra invalid cases that Atoi can handle sanely:
+	// - "i-0e": -0 is invalid
+	// - "i03e": leading 0s are invalid, except for "i0e"
+	//
 	// The maximum number of bits is unspecified. But 64-bit integers can handle very big numbers.
 
-	return strconv.ParseInt(bencodedString[1:len(bencodedString)-1], 10, 64)
+	num, err := strconv.Atoi(bencData[1 : nextIndex-1])
+	if err != nil {
+		return 0, 0, err
+	} else {
+		return num, uint(nextIndex), nil
+	}
 }
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	// fmt.Println("Logs from your program will appear here!")
-
 	command := os.Args[1]
 
 	if command == "decode" {
